e0b7540a5dded7b176092b11784eff488f5718c8
Kirill <Kirill@GEG-PC>
2023-12-21 19:35:13 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new WinFormsControls.TransparentLabel();
+            this.label1 = new WinFormsControls.TransparentLabel();
+            this.transparentLabel1 = new WinFormsControls.TransparentLabel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(587, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 72);
+            this.button1.TabIndex = 5;
+            this.button1.TabStop = false;
+            this.button1.Text = "TEXT";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(1, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 32);
+            this.label1.TabIndex = 4;
+            this.label1.TabStop = false;
+            this.label1.Text = "TEXT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            // 
+            // transparentLabel1
+            // 
+            this.transparentLabel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.transparentLabel1.BackgroundImage = global::WindowsFormsApp3.Properties.Resources.loadbar;
+            this.transparentLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.transparentLabel1.ForeColor = System.Drawing.SystemColors.Control;
+            this.transparentLabel1.Location = new System.Drawing.Point(0, 405);
+            this.transparentLabel1.Name = "transparentLabel1";
+            this.transparentLabel1.Size = new System.Drawing.Size(581, 32);
+            this.transparentLabel1.TabIndex = 7;
+            this.transparentLabel1.TabStop = false;
+            this.transparentLabel1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 386);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(0, 72);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp3.Properties.Resources.background;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(868, 457);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 457);
+            this.Controls.Add(this.transparentLabel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private WinFormsControls.TransparentLabel label1;
+        private WinFormsControls.TransparentLabel button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private WinFormsControls.TransparentLabel transparentLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using WinFormsControls;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string version = "";
+        public string serverVersion = "";
+        public string path = @"Game\";
+        public string exename = "Lethal Company.exe";
+        public string url = "https://octoland.ru/games/lc/";
+        public bool downloading = false;
+
+        PrivateFontCollection pfc = new PrivateFontCollection();
+        public void InitFont()
+        {
+            pfc.AddFontFile("Chava.ttf");
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitFont();
+            //pictureBox1.BackgroundImage = new Bitmap("background.jpg");
+            pictureBox2.BackgroundImage = new Bitmap("loadbar.png");
+            label1.Font = new Font(pfc.Families[0], 20, FontStyle.Regular);
+            transparentLabel1.Font = new Font(pfc.Families[0], 20, FontStyle.Regular);
+            label1.BringToFront();
+            button1.Font = new Font(pfc.Families[0], label1.Font.Size);
+
+
+
+            Directory.CreateDirectory(Path.GetFullPath(path));
+            if (!File.Exists(Path.GetFullPath(path + exename)))
+            {
+                RegistryKey key;
+                key = Registry.CurrentUser.CreateSubKey("lethalLauncher");
+                key.SetValue("version", "none");
+                key.Close();
+                version = "none";
+            }
+            else
+            {
+                version = (string)Registry.CurrentUser.OpenSubKey("lethalLauncher").GetValue("version");
+            }
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + "v.txt");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream recivestream = response.GetResponseStream();
+                StreamReader readstream = null;
+                if (response.CharacterSet == null)
+                {
+                    readstream = new StreamReader(recivestream);
+                }
+                else
+                {
+                    readstream = new StreamReader(recivestream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readstream.ReadToEnd();
+                serverVersion = data;
+                response.Close();
+                readstream.Close();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+
+            label1.Text = version + "/" + serverVersion;
+            if (!downloading)
+            {
+                if (version != serverVersion)
+                {
+                    if (File.Exists(Path.GetFullPath(path + exename)))
+                    {
+                        button1.Text = "Update to " + serverVersion;
+                        button1.Click += UpdateGame;
+                    }
+                    else
+                    {
+                        button1.Text = "Download";
+                        button1.Click += UpdateGame;
+                    }
+                }
+                else
+                {
+                    button1.Text = "Play";
+                    button1.Click += Play;
+                }
+            }
+        }
+        
+        private void UpdateGame(object sender, EventArgs e)
+        {
+            downloading = false;
+            button1.ForeColor = Color.Gray;
+            button1.Enabled = false;
+            DirectoryInfo dir = new DirectoryInfo(Path.GetFullPath(path));
+            foreach (FileInfo item in dir.GetFiles())
+            {
+                item.Delete();
+            }
+            foreach (DirectoryInfo item in dir.GetDirectories())
+            {
+                item.Delete(true);
+            }
+
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += (s, g) => { 
+                    if (g.ProgressPercentage == 100)
+                    {
+                        if (!File.Exists(Path.GetFullPath(path + exename)))
+                        {
+                            ZipFile.ExtractToDirectory("game.zip", path);
+                            RegistryKey key;
+                            key = Registry.CurrentUser.CreateSubKey("lethalLauncher");
+                            key.SetValue("version", serverVersion);
+                            key.Close();
+                            version = serverVersion;
+                            label1.Text = version + "/" + serverVersion;
+
+                            button1.Text = "Play";
+                            button1.Click -= UpdateGame;
+                            button1.Click += Play; ;
+
+                            button1.ForeColor = Color.White;
+                        }
+                        button1.Enabled = true;
+                        downloading = false;
+                    }
+                    transparentLabel1.Text = (g.BytesReceived / 1024) + "/" + (g.TotalBytesToReceive / 1024);
+                    pictureBox2.Width = (int)(591f * (g.ProgressPercentage / 100f));
+                };
+                wc.DownloadFileAsync(new Uri(url + "current.zip"), "game.zip");
+            }
+        }
+
+        private void Play(object sender, EventArgs e)
+        {
+            Process.Start(path + exename);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
